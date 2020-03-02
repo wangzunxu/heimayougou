880f85a0baef6c8c73b05cb0110de0b251955d3d
wzx <546179982@qq.com>
2020-03-02 21:29:15 +0800

    搜索框和轮播图

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 //app.js
+import request from "./utils/request.js"
 App({
   onLaunch: function () {
+    // 指定基准路径
+    request.defualts.baseURL = "https://api-hmugo-web.itheima.net/api/public/v1"
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,19 @@
-
+import request from "../../utils/request.js"
 Page({
-
+data: {
+  // 轮播图数据
+  banners:[
+  ]
+},
+onLoad() {
+  // 请求轮播图接口
+request({
+  url:"/home/swiperdata"
+}).then(res=>{
+  console.log(res)
+  this.setData({
+    banners:res.data.message
+  })
+})
+}
 })
