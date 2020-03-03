89ee3d093dff8414e5e954fe1eb838eb70deb8a9
wzx <546179982@qq.com>
2020-03-03 22:45:55 +0800

    分类页面渲染

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,18 +1,27 @@
 // pages/category/index.js
+import request from "../../utils/request.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    current:0
+    current:0,
+    menuList:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    request({
+      url:"/categories"
+    }).then(res=>{
+      console.log(res)
+      this.setData({
+        menuList:res.data.message
+      })
+    })
   },
   handleClick(e) {
     const {index} = e.currentTarget.dataset;
