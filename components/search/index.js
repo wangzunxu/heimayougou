53a9f79aaa1f0ae683859283ef0ecaa8c0c1914e
wzx <546179982@qq.com>
2020-03-07 00:06:09 +0800

    自定义组件

--- components/search/index.js
+++ components/search/index.js
@@ -4,8 +4,16 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    keyword:{
+      type: String,
+      value: '搜索'//默认值
+    },
+    align:{
+      type:String,
+      value:'center'
+    }
   },
+  externalClasses:['background',"align"],
 
   /**
    * 组件的初始数据
