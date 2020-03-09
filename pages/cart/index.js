1dc1962d86adf39bfd71b7e868226e6e3af02f67
wzx <546179982@qq.com>
2020-03-09 21:19:12 +0800

    当商品数量为0时删除商品

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -6,7 +6,8 @@ Page({
    */
   data: {
     address:{},
-    goods: []
+    goods: [],
+    allPrice:0
   },
 
   /**
@@ -24,8 +25,47 @@ Page({
     this.setData({
       goods:wx.getStorageSync("goods") || []
     })
+    this.handleAllPrice();
   },
+  handleCalc(e) {
+    const {index,number} = e.currentTarget.dataset;
+    console.log(number)
+    this.data.goods[index].number+=number;
 
+    // 如果数量为零，则删除该商品
+    if(this.data.goods[index].number===0) {
+      //弹出
+      wx.showModal({
+        title: '提示',
+        content: '是否删除该商品',
+        success:(res)=> {
+          if (res.confirm) {
+            // console.log(this.data.goods)
+            this.data.goods.splice(index,1)
+            // 删除就刷新
+            this.setData({
+              goods: this.data.goods
+            })
+          }
+        }
+      })
+    }
+    // 调用setData刷新页面
+    // 用户点击 + 或者 - 号就按顺序执行了，而wx:showModal中的success
+    // 是在点击确认后才执行的，所以先执行的下面后执行上面的success
+    this.setData({
+      goods:this.data.goods
+    })
+  },
+  handleAllPrice() {
+    let price = 0;
+    this.data.goods.forEach(v=>{
+      price += v.goods_price*v.number;
+    })
+    this.setData({
+      allPrice:price
+    })  
+    },
 /**
  * 获取收货地址
  */
