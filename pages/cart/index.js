b89fe43b98be4544ad3c9b79a970a253771d14a8
wzx <546179982@qq.com>
2020-03-09 13:23:45 +0800

    用户信息存储本地

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,62 +5,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    address:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 获取本地地址
+    if(!this.data.address.name) {
+      this.data.address = {};
+    }
+    let address =  wx.getStorageSync("address")
+    this.setData({
+      address 
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+/**
+ * 获取收货地址
+ */
+  handleGetAddress () {
+    wx.chooseAddress({
+      success:res => {
+        // console.log(res)
+        this.setData({
+          address:{
+            name:res.userName,
+            tel:res.telNumber,
+            detail:res.cityName+res.countyName+res.detailInfo
+          }
+        });
+        // 保存信息到本地
+        wx.setStorageSync("address",this.data.address)
+      }
+    }) 
   }
 })
